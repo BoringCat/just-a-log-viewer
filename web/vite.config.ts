f6b79e1585057889890b175de864ff4cfc525c05
boringcat <boringcat@outlook.com>
2025-05-19 22:10:30 +0800

    优化打包

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,8 +9,39 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
 
+const libraryChunkMap:{[key:string]:string} = {
+  "element-plus": "element-plus",
+  vue: "vue",
+};
+
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 创建一个对象映射，用于存储库名及其对应的chunk名称
+          // 检查模块ID是否包含'node_modules'，即是否为第三方依赖
+          if (id.includes("node_modules")) {
+            // 遍历libraryChunkMap的键（即库名），查找与模块ID存在包含关系的库名
+            const matchedLibrary = Object.keys(libraryChunkMap).find((library) =>
+              id.includes(library)
+            );
+            // 如果找到了匹配的库名，返回对应的chunk名称（从libraryChunkMap中获取）
+            if (matchedLibrary) {
+              return libraryChunkMap[matchedLibrary];
+            } else {
+              // 如果未找到匹配的库名，将该第三方依赖归入默认的'vendor' chunk
+              return "vendor";
+            }
+          } else {
+            // 如果模块ID不包含'node_modules'，即不是第三方依赖，则将其归入'index' chunk
+            return "index";
+          }
+        },
+      }
+    }
+  },
   server: {
     proxy: {
       '/api/v1': 'http://localhost:8514'
@@ -25,7 +56,7 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    }),
+    })
   ],
   resolve: {
     alias: {
